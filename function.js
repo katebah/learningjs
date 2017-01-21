98a5e1acae8c7725d1288df25ccd652e23428925
katebah <katebah.alolefi@berkeley.edu>
2017-01-21 01:02:51 -0800

    Session with Chris On Functions

--- function.js
+++ function.js
@@ -7,12 +7,18 @@ Date: 1/6/17
 */
 
 // set array 
-var fruits2 = GetMeFruits(['orange','guava','grapes'])
-console.log (fruits2)
+//var pals = GetMeFriends(['sam','ali','nik'])
+//var pals2 = GetMeFriends ( [ 'ahmed', 'hamza', 'mohammed'])
+console.log (AddLastName(['khawla']))
+console.log (AddLastName(['marwat']))
+console.log (AddLastName(['mohammed']))
+console.log (GetMeTheDate())
 //Return string of fruits
-function GetMeFruits(more_fruits) {
-	var fruits  = ['apple', 'kiwi', 'pineapple']
-	var more = fruits.concat(more_fruits) // concat both arrays
-	message = more.join(' ') // turn array into string
-	return message // return string
+function AddLastName(more_pals) {
+	return more_pals.concat(['alolefi']).join (' ') // concat both arrays
+	//message = results.join(' ') // turn array into string
+	//return results // return string
 }
+function GetMeTheDate ()
+{ return new Date()}
+
